198251b02eb4818a78a5d70de3cda86dc522d0c2
Sarah <sarahlord79@gmail.com>
2026-03-10 12:59:54 -0500

    updated team week2 step 6 and 7

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,10 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
 
-console.log(dataSource.findProductById(productId));
+const product = new ProductDetails(productId, dataSource);
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+product.init();
