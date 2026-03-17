f5fae3e27bc5aae0a359c40511a16bb28f9d4a00
Sarah <sarahlord79@gmail.com>
2026-03-17 16:14:36 -0500

    Week 3 team activity

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@ import ProductData from "./ProductData.mjs";
 
 import ProductList from "./ProductList.mjs";
 
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
+
 const dataSource = new ProductData('tents');
 const listElement = document.querySelector(".product-list");
 const myList = new ProductList("tents", dataSource, listElement);
